4fbff719100b3f79e07eba308239ae02a55c329d
Vilakhan <vilakhans@gmail.com>
2016-12-13 16:56:34 +0700

    Finish 13dec

--- app/src/main/java/ltc/vilakhan/traingwhere/SignUpActivity.java
+++ app/src/main/java/ltc/vilakhan/traingwhere/SignUpActivity.java
@@ -2,12 +2,47 @@ package ltc.vilakhan.traingwhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    //Explicit ປະກາດຕົວປ່ຽນ
+    private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+    } //  Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+
+            // Have space
+            Log.d("13decV1", "Have Space");
+        }
+
+
+    } // Click sign
+
+
+} // Main class
