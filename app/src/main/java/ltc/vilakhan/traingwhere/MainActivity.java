884f3e3a6984b36176b64d36fce2bdf66d829ee3
Vilakhan <vilakhans@gmail.com>
2016-12-14 15:23:59 +0700

    Add Content

--- app/src/main/java/ltc/vilakhan/traingwhere/MainActivity.java
+++ app/src/main/java/ltc/vilakhan/traingwhere/MainActivity.java
@@ -3,15 +3,46 @@ package ltc.vilakhan.traingwhere;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initInstance();
+
+
     } // Main method
+
+    private void initInstance() {
+        // Bind widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+    }
+
+    public void clickSignIn(View view) {
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("")||passwordString.equals("")) {
+            // Have space
+            MyAlert myAlert = new MyAlert(MainActivity.this, "Have Space",
+                    "Pleae fill all blank", R.drawable.doremon48);
+            myAlert.myDialog();
+        } else {
+            // No space
+
+        }
+    } // Click sign in
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
