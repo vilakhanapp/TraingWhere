012f78a9d533f672fccf6d0abea9ee74a6d46c70
Vilakhan <vilakhans@gmail.com>
2016-12-14 17:04:48 +0700

    Authen Finish

--- app/src/main/java/ltc/vilakhan/traingwhere/MainActivity.java
+++ app/src/main/java/ltc/vilakhan/traingwhere/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     // Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
-
+    private String[] loginStrings;
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +38,66 @@ public class MainActivity extends AppCompatActivity {
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
-        if (userString.equals("")||passwordString.equals("")) {
+        if (userString.equals("") || passwordString.equals("")) {
             // Have space
-            MyAlert myAlert = new MyAlert(MainActivity.this, "Have Space",
-                    "Pleae fill all blank", R.drawable.doremon48);
+            MyAlert myAlert = new MyAlert(MainActivity.this, getResources().getString(R.string.title_have_space),
+                    getResources().getString(R.string.message_have_space), R.drawable.nobita48);
             myAlert.myDialog();
         } else {
             // No space
+            try {
+
+                SynUser synUser = new SynUser(MainActivity.this);
+                synUser.execute();
+                String s = synUser.get();
+                Log.d("14decV2", "JSon ==> " + s);
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                loginStrings = new String[4];
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.getString("User"))) {
+                        loginStrings[0] = jsonObject.getString("id");
+                        loginStrings[1] = jsonObject.getString("Name");
+                        loginStrings[2] = jsonObject.getString("User");
+                        loginStrings[3] = jsonObject.getString("Password");
+                        aBoolean = false;
+                    }
+                } // for
+
+                if (aBoolean) {
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            getResources().getString(R.string.title_user_false),
+                            getResources().getString(R.string.message_user_false),
+                            R.drawable.rat48);
+                    myAlert.myDialog();
+                } else if (passwordString.equals(loginStrings[3])) {
+                    //Password true
+                    Toast.makeText(MainActivity.this, "Welcome " + loginStrings[1],
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    //Password false
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            getResources().getString(R.string.title_password_false),
+                            getResources().getString(R.string.message_pass_false),
+                            R.drawable.kon48);
+                    myAlert.myDialog();
+                }
+
+            } catch (Exception e) {
+
+                Log.d("14decV2", "e Main ==> " + e.toString());
+            }
 
         }
     } // Click sign in
 
     public void clickSignUpMain(View view) {
-        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 
 } // Main class
---
+++ app/src/main/java/ltc/vilakhan/traingwhere/SynUser.java
@@ -0,0 +1,43 @@
+package ltc.vilakhan.traingwhere;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by Boualy on 12/14/2016.
+ */
+
+public class SynUser extends AsyncTask<Void,Void,String>{
+
+    //Explicit
+    private Context context;
+    private static final String urlJSON = "http://swiftcodingthai.com/ltc/get_user_ka.php";
+
+    public SynUser(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(urlJSON).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+        } catch (Exception e) {
+            Log.d("14decV2", "e doin ==>" + e.toString());
+            return null;
+        }
+
+
+    }
+} // Main class
